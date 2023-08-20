eab45d06d186fb0076535488e142f34b3d4d31b8
dhershman1 <dustinh17@gmail.com>
2023-08-20 13:49:40 -0400

    Updated: Move blueprints to blueprint page

--- src/views/Blueprints.vue
+++ src/views/Blueprints.vue
@@ -1,5 +1,62 @@
 <template>
   <div class="blueprints">
-    <h1>This is the blueprints page</h1>
+    <account-card />
+    <div class="filters">
+      <basic-card :title="'Filters?'">
+        <template #content>
+          <p>Hewwo</p>
+        </template>
+      </basic-card>
+    </div>
+    <div class="previews">
+      <div>
+        <preview-card>
+          <template #preview-content>
+            <h1>Tavern Build</h1>
+            <h2>It's a Tavern! Whoa!</h2>
+            <p>Something something, booze and unlimited power!</p>
+          </template>
+        </preview-card>
+      </div>
+      <div>
+        <preview-card>
+          <template #preview-content>
+            <h1>Tavern Build</h1>
+            <h2>It's a Tavern! Whoa!</h2>
+            <p>Something something, booze and unlimited power!</p>
+          </template>
+        </preview-card>
+      </div>
+      <div>
+        <preview-card>
+          <template #preview-content>
+            <h1>Tavern Build</h1>
+            <h2>It's a Tavern! Whoa!</h2>
+            <p>Something something, booze and unlimited power!</p>
+          </template>
+        </preview-card>
+      </div>
+    </div>
+    <pagination />
   </div>
 </template>
+
+<script setup>
+import AccountCard from '../components/cards/Account.vue'
+import BasicCard from '../components/cards/Basic.vue'
+import Pagination from '../components/Pagination.vue'
+import PreviewCard from '../components/cards/Preview.vue'
+</script>
+
+<style scoped>
+.previews {
+  grid-area: previews;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 1rem;
+}
+
+.filters {
+  grid-area: news;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,31 @@
 <template>
   <div class="home">
-    <account-card />
-    <div class="news">
-      <basic-card
-        :title="'Filters?'"
-      >
+    <basic-card :title="'News'">
         <template #content>
-          <p>Hewwo</p>
+          <p>News</p>
         </template>
-      </basic-card>
-    </div>
-    <div class="previews">
-      <div>
-        <preview-card>
-          <template #preview-content>
-            <h1>Tavern Build</h1>
-            <h2>It's a Tavern! Whoa!</h2>
-            <p>Something something, booze and unlimited power!</p>
-          </template>
-        </preview-card>
-      </div>
-      <div>
-        <preview-card>
-          <template #preview-content>
-            <h1>Tavern Build</h1>
-            <h2>It's a Tavern! Whoa!</h2>
-            <p>Something something, booze and unlimited power!</p>
-          </template>
-        </preview-card>
-      </div>
-      <div>
-        <preview-card>
-          <template #preview-content>
-            <h1>Tavern Build</h1>
-            <h2>It's a Tavern! Whoa!</h2>
-            <p>Something something, booze and unlimited power!</p>
-          </template>
-        </preview-card>
-      </div>
-    </div>
-    <pagination />
+    </basic-card>
+    <basic-card :title="'Most Recent Blueprints'">
+      <template #content>
+        <p>Most Recent Blueprints</p>
+      </template>
+    </basic-card>
   </div>
 </template>
 
 <script setup>
-import AccountCard from '../components/cards/Account.vue'
 import BasicCard from '../components/cards/Basic.vue'
-import Pagination from '../components/Pagination.vue'
-import PreviewCard from '../components/cards/Preview.vue'
 </script>
 
 <style scoped>
+.card:first {
+  margin-top: inherit;
+}
+
+.card {
+  margin-top: 1rem;
+}
+
 .previews {
   grid-area: previews;
   display: grid;
